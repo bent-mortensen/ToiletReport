a54fbe825f77b630f08de91a289a69645f4267eb
Bent Sunesen Mortensen <kooogt@gmail.com>
2018-05-16 08:58:24 +0200

    home report about done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router 
+  ) { }
 
   ngOnInit() {
   }
 
+  GoToHome(){
+    this.router.navigate(["/home"]);
+  }
 }
